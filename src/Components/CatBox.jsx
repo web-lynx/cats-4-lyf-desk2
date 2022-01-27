53eecaa68484b09ad5dc7858aa35621472f66937
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-01-27 12:07:03 +0000

    (ARW) added appropriate HTML to catbox component, as well as random cat price generator and random cat name via cat-names package

--- src/Components/CatBox.jsx
+++ src/Components/CatBox.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 export const CatBox = () => {
   const [cat, setCat] = useState({});
+  const catPrice = (Math.random() * 10).toFixed(2)
+  const catNames = require('cat-names');
 
   const fetchCat = async () => {
     const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=1");
     const dataArray = await response.json();
-    console.log(dataArray);
-    
+    console.log(dataArray[0]);
     setCat(dataArray[0]);
   };
 
@@ -16,8 +17,12 @@ export const CatBox = () => {
   }, []);
   
   return (
-    <div>
-      <img src={cat.url} alt="a cat" />
+    <div className="catbox">
+      <h3 className="catbox-title">{catNames.random()}</h3>
+      <img className="catbox-image" src={cat.url} alt="a cat" />
+      <p className="cat-price">£{catPrice}</p>
+      <button className="basket-button">Add to basket</button>
+      <button className="remove-basket-button">Remove from basket</button>
     </div>
   );
 };
