753a23276c249d3a99abd85020ba79618073d47a
DannyABoat <dannysum07@yahoo.com>
2022-02-07 13:53:12 +0000

    new functions added to make app more interactive

--- src/Components/CatBox.jsx
+++ src/Components/CatBox.jsx
@@ -1,28 +1,21 @@
-import React, { useState, useEffect } from 'react';
-
-export const CatBox = () => {
-  const [cat, setCat] = useState({});
-  const catPrice = (Math.random() * 10).toFixed(2)
-  const catNames = require('cat-names');
-
-  const fetchCat = async () => {
-    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=1");
-    const dataArray = await response.json();
-    console.log(dataArray[0]);
-    setCat(dataArray[0]);
-  };
-
-  useEffect(() => {fetchCat();}, []);
-  
+const CatBox = ({ image, addToBasket, catName, price }) => {
   return (
     <div className="catbox">
       <div>
-        <h3 className="catbox-title">{catNames.random()}</h3>
-        <img className="catbox-image" src={cat.url} alt="a cat" />
-        <p className="cat-price">£{catPrice}</p>
-        <button className="basket-button">Add to basket</button>
-        <button className="remove-basket-button">Remove from basket</button>
+        <h3>{catName}</h3>
+        <img src={image} alt="a cat" />
+        <p>£{price}</p>
+        <button
+          onClick={() => {
+            addToBasket({ catName, price, image });
+          }}
+        >
+          Add to basket
+        </button>
+        <button>Remove from basket</button>
       </div>
     </div>
   );
 };
+
+export default CatBox;
--- src/Components/CatModal.jsx
+++ src/Components/CatModal.jsx
@@ -1,30 +1,35 @@
-import React,{ useState } from 'react' 
-import Modal from 'react-modal';
-import CatContainer from './CatContainer';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
-function CatModal (){
+function CatModal({ basketItems }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-    const [modalIsOpen,setModalIsOpen] = useState(false);
+  const setModalIsOpenToTrue = () => {
+    setModalIsOpen(true);
+  };
 
-    const setModalIsOpenToTrue =()=>{
-        setModalIsOpen(true)
-    }
+  const setModalIsOpenToFalse = () => {
+    setModalIsOpen(false);
+  };
 
-    const setModalIsOpenToFalse =()=>{
-        setModalIsOpen(false)
-    }
+  return (
+    <div className="CatModal" id="cat-modal">
+      <button id="modal-button" onClick={setModalIsOpenToTrue}>
+        Open Basket
+      </button>
 
-    return(
-        <div className='CatModal'>
-            <button onClick={setModalIsOpenToTrue}>Click to Open Modal</button>
-
-            <Modal isOpen={modalIsOpen}>
-                <button onClick={setModalIsOpenToFalse}>x</button>
-                <CatContainer/>
-
-            </Modal>
-        </div>
-    )
+      <Modal isOpen={modalIsOpen}>
+        <button onClick={setModalIsOpenToFalse}>Close Basket</button>
+        {basketItems.map(({ catName, image, price }) => (
+          <div>
+            <h3>{catName}</h3>
+            <img src={image} alt="a cat" />
+            <p>£{price}</p>
+          </div>
+        ))}
+      </Modal>
+    </div>
+  );
 }
 
 export default CatModal;
